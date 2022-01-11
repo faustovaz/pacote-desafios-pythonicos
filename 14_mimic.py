1cf196f16819ee934854255c668737509df49d5e
Jefferson Fausto Vaz <faustovaz@gmail.com>
2022-01-10 23:26:37 -0300

    Solved problem 14

--- 14_mimic.py
+++ 14_mimic.py
@@ -42,29 +42,54 @@ método que escolhe um elemento aleatório de uma lista não vazia.
 import random
 import sys
 
+def get_words(filename):
+    """Retorna as palavaras do arquivo"""
+    words = []
+    with open(filename, 'r', encoding='utf-8') as file:
+        line =  file.readline()
+        while line:
+            line_words = line.split(" ")
+            line_words = [word.strip("()\n`-,.:'").replace("-", " ").lower()
+                for word in line_words if len(word) > 1]
+            words.extend(line_words)
+            line = file.readline()
+    return words
 
 def mimic_dict(filename):
-  """Retorna o dicionario imitador mapeando cada palavra para a lista de
-  palavras subsequentes."""
-    # +++ SUA SOLUÇÃO +++
-  return
+    """Retorna o dicionario imitador mapeando cada palavra para a lista de
+    palavras subsequentes."""
+    file_words = get_words(filename)
+    m_dict = {'': [file_words[0]]}
+    for i, word in enumerate(file_words):
+        words = m_dict.get(word, [])
+        next_word_index = i + 1
+        if next_word_index < len(file_words):
+            if file_words[next_word_index] not in words:
+                words.append(file_words[next_word_index])
+        else:
+            words.append('')
+        m_dict[word] = words
+    return m_dict
 
-
-def print_mimic(mimic_dict, word):
-  """Dado o dicionario imitador e a palavra inicial, imprime texto de 200 palavras."""
-    # +++ SUA SOLUÇÃO +++
-  return
+def print_mimic(words_dict, word):
+    """Dado o dicionario imitador e a palavra inicial, imprime texto de 200 palavras."""
+    word = words_dict[''][0]
+    words = [word]
+    for i in range(199):
+        word = random.choice(words_dict.get(word))
+        words.append(word)
+    print(" ".join(words))
 
 
 # Chama mimic_dict() e print_mimic()
 def main():
-  if len(sys.argv) != 2:
-    print('Utilização: ./14_mimic.py file-to-read')
-    sys.exit(1)
+    if len(sys.argv) != 2:
+        print('Utilização: ./14_mimic.py file-to-read')
+        sys.exit(1)
 
-  dict = mimic_dict(sys.argv[1])
-  print_mimic(dict, '')
+    m_dict = mimic_dict(sys.argv[1])
+    print_mimic(m_dict, '')
 
 
 if __name__ == '__main__':
-  main()
+    main()
